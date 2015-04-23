16bd4784cd5cb04cacfc0794449c1154df46a9cf
Steven Bussert & Ian Bevel <student@epicodus.com>
2015-04-23 11:55:49 -0700

    Refactoring.

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -6,36 +6,68 @@ class Fixnum
     coin_words = ""
     total_coins=self
     counter = 0
-
-    until total_coins.div(25) == counter
-        counter += 1
+    indexcounter = 0
+    coin_values = [25,10,5]
+    coin_values_words = ["quarter","dime","nickle"]
+    while total_coins > 4
+      until total_coins.div(coin_values[indexcounter]) == counter
+        counter+= 1
+      end
+      if counter > 1
+        coin_words += " " + counter.to_s + " " + coin_values_words[indexcounter] + "s"
+      elsif counter > 0
+        coin_words += " " + counter.to_s + " " + coin_values_words[indexcounter]
+      end
+      total_coins -= counter * coin_values[indexcounter]
+      indexcounter += 1
+      counter = 0
     end
-    if counter > 0
-      coin_words += counter.to_s + " quarters "
-    end
-    total_coins -= counter*25
-    counter = 0
-
-    until total_coins.div(10) == counter
-        counter += 1
-    end
-    if counter > 0
-      coin_words += counter.to_s + " dimes "
-    end
-    total_coins -= counter*10
-    counter = 0
-
-    until total_coins.div(5) == counter
-        counter += 1
-    end
-    if counter > 0
-      coin_words += counter.to_s + " nickles "
-    end
-    total_coins -= counter*5
-
-    if total_coins > 0
-      coin_words += total_coins.to_s + " pennies"
+    if total_coins > 1
+      coin_words += " " + total_coins.to_s + " pennies"
+    elsif total_coins == 1
+      coin_words += total_coins.to_s + " penny"
     end
     coin_words
   end
 end
+
+
+# class Fixnum
+#   define_method(:coin_combos) do
+#     coin_words = ""
+#     total_coins=self
+#     counter = 0
+#     until total_coins.div(25) == counter
+#         counter += 1
+#     end
+#     if counter > 0
+#       coin_words += counter.to_s + " quarters "
+#     end
+#     total_coins -= counter*25
+#     counter = 0
+#
+#     until total_coins.div(10) == counter
+#         counter += 1
+#     end
+#     if counter > 0
+#       coin_words += counter.to_s + " dimes "
+#     end
+#     total_coins -= counter*10
+#     counter = 0
+#
+#     until total_coins.div(5) == counter
+#         counter += 1
+#     end
+#     if counter > 0
+#       coin_words += counter.to_s + " nickles "
+#     end
+#     total_coins -= counter*5
+#
+#     if total_coins > 1
+#       coin_words += " " + total_coins.to_s + " pennies"
+#     elsif total_coins == 1
+#       coin_words += total_coins.to_s + " penny"
+#     end
+#     coin_words
+#   end
+# end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -3,15 +3,15 @@ require('coin_combos')
 
 describe('Fixnum#coin_combos') do
   it('for a given number of cents returns correct number of pennies') do
-    expect(1.coin_combos).to(eq("1 pennies"))
+    expect(2.coin_combos).to(eq(" 2 pennies"))
   end
   it('for a given number of cents returns correct number of nickles and pennies') do
-    expect(5.coin_combos).to(eq("1 nickles "))
+    expect(5.coin_combos).to(eq(" 1 nickle"))
   end
   it('for a given number of cents returns correct number of dimes, nickles, and pennies') do
-    expect(10.coin_combos).to(eq("1 dimes "))
+    expect(20.coin_combos).to(eq(" 2 dimes"))
   end
   it('for a given number of cents returns correct number of quarters, dimes, nickles, and pennies') do
-    expect(25.coin_combos).to(eq("1 quarters "))
+    expect(50.coin_combos).to(eq(" 2 quarters"))
   end
 end
