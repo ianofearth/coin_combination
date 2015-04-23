87714dbc28f0d5b234692f61d8e47371641d774e
Steven Bussert & Ian Bevel <student@epicodus.com>
2015-04-23 11:32:34 -0700

    Gets coin_combo program working and tested.

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -1,11 +1,41 @@
+require ('pry')
+
+
 class Fixnum
   define_method(:coin_combos) do
     coin_words = ""
+    total_coins=self
     counter = 0
-    until counter == (self)
-      puts counter.to_s + " pennies"
-      counter += 1
+
+    until total_coins.div(25) == counter
+        counter += 1
     end
-    coin_words += counter.to_s + " pennies"
+    if counter > 0
+      coin_words += counter.to_s + " quarters "
+    end
+    total_coins -= counter*25
+    counter = 0
+
+    until total_coins.div(10) == counter
+        counter += 1
+    end
+    if counter > 0
+      coin_words += counter.to_s + " dimes "
+    end
+    total_coins -= counter*10
+    counter = 0
+
+    until total_coins.div(5) == counter
+        counter += 1
+    end
+    if counter > 0
+      coin_words += counter.to_s + " nickles "
+    end
+    total_coins -= counter*5
+
+    if total_coins > 0
+      coin_words += total_coins.to_s + " pennies"
+    end
+    coin_words
   end
 end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -5,6 +5,13 @@ describe('Fixnum#coin_combos') do
   it('for a given number of cents returns correct number of pennies') do
     expect(1.coin_combos).to(eq("1 pennies"))
   end
-
-
+  it('for a given number of cents returns correct number of nickles and pennies') do
+    expect(5.coin_combos).to(eq("1 nickles "))
+  end
+  it('for a given number of cents returns correct number of dimes, nickles, and pennies') do
+    expect(10.coin_combos).to(eq("1 dimes "))
+  end
+  it('for a given number of cents returns correct number of quarters, dimes, nickles, and pennies') do
+    expect(25.coin_combos).to(eq("1 quarters "))
+  end
 end
